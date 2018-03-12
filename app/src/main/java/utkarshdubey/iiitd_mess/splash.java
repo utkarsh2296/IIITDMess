8a568b76d5f8fa1ee779aadfeb2083a3e253224d
utkarsh2296 <dubeyutkarsh2296@gmail.com>
2018-03-12 15:49:00 +0530

    mine done complaints left ( om ) prabhat add qr and logout

---
+++ app/src/main/java/utkarshdubey/iiitd_mess/splash.java
@@ -0,0 +1,26 @@
+package utkarshdubey.iiitd_mess;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            public void run()
+            {
+                Intent i=new Intent(splash.this,Googlesignin.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
+    }
+    }
+}
